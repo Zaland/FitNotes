a58590e22d289e989c92ed317fe4c97e206f7a31
Zaland Khan <zalandkhan1@gmail.com>
2022-06-09 20:39:34 -0700

    Add supertokens to api

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,24 +1,57 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
+import supertokens from "supertokens-node";
+import Session from "supertokens-node/recipe/session";
+import EmailPassword from "supertokens-node/recipe/emailpassword";
+import { middleware, errorHandler } from "supertokens-node/framework/express";
+import dotenv from "dotenv";
 
+// init env values
+dotenv.config();
+
+// init express app
 const app = express();
+
+// init supertokens
+supertokens.init({
+  framework: "express",
+  supertokens: {
+    connectionURI: process.env.CONNECTION_URI,
+    apiKey: process.env.API_KEY,
+  },
+  appInfo: {
+    appName: "FitNotes",
+    apiDomain: process.env.API_DOMAIN,
+    websiteDomain: process.env.CLIENT_DOMAIN,
+    apiBasePath: "/auth",
+    websiteBasePath: "/auth",
+  },
+  recipeList: [EmailPassword.init(), Session.init()],
+});
 
 // bodyparser
 app.use(bodyParser.json());
 
 // cors
-app.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: process.env.CLIENT_DOMAIN,
+    allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+    credentials: true,
+  })
+);
 
+// supertokens middleware
+app.use(middleware());
+
+// endpoints
 app.get("/help", async (_req, _res, next) => {
   console.log("help");
   next();
 });
+
+// supertokens error handler
+app.use(errorHandler());
 
 export default app;

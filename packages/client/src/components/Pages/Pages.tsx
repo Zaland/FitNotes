5d9cbd6342a1d3153a85d3705b094141d1b4a1db
Zaland Khan <zalandkhan1@gmail.com>
2022-06-17 20:03:17 -0700

    Add global theme

--- packages/client/src/components/Pages/Pages.tsx
+++ packages/client/src/components/Pages/Pages.tsx
@@ -1,23 +1,27 @@
+import { ThemeProvider } from "@mui/material";
 import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
 import { EmailPasswordAuth } from "supertokens-auth-react/recipe/emailpassword";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import * as reactRouterDom from "react-router-dom";
 
 import { Navbar } from "../Navbar";
+import { theme } from "./styles";
 
 export const Pages = () => (
-  <BrowserRouter>
-    <Routes>
-      {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+        {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
 
-      <Route
-        path="/"
-        element={
-          <EmailPasswordAuth>
-            <Navbar />
-          </EmailPasswordAuth>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
+        <Route
+          path="/"
+          element={
+            <EmailPasswordAuth>
+              <Navbar />
+            </EmailPasswordAuth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );

0c8c2f3abe51bc8905d65b5d5272ad32ce3067a9
Zaland Khan <zalandkhan1@gmail.com>
2022-07-16 15:52:34 -0700

    Add settings page with chakra ui

--- client/src/components/Pages/Settings/Settings.tsx
+++ client/src/components/Pages/Settings/Settings.tsx
@@ -1,3 +1,39 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Stack,
+  Heading,
+  FormControl,
+  FormLabel,
+  Switch,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
-export const Settings = () => <Box p={4}>Settings</Box>;
+export const Settings = () => (
+  <Flex
+    minH="100vh"
+    justify="center"
+    bg={useColorModeValue("gray.50", "gray.800")}
+  >
+    <Stack spacing={8} mx="auto" maxW="lg" minW="md" py={12} px={6}>
+      <Stack align="center">
+        <Heading fontSize="4xl">Settings</Heading>
+      </Stack>
+      <Box
+        rounded="lg"
+        bg={useColorModeValue("white", "gray.700")}
+        boxShadow="lg"
+        p={8}
+      >
+        <Stack spacing={4}>
+          <FormControl display="flex" alignItems="center">
+            <Switch colorScheme="purple" id="dark-mode" />
+            <FormLabel htmlFor="dark-mode" ml={3} mb={-1}>
+              Dark mode
+            </FormLabel>
+          </FormControl>
+        </Stack>
+      </Box>
+    </Stack>
+  </Flex>
+);

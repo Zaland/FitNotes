5ddc02a1b740dd222303ebad5dedb7a0ec920395
Zaland Khan <zalandkhan1@gmail.com>
2022-08-03 19:33:50 -0700

    Fix route not loading content

--- client/src/components/Pages/Auth/PrivateRoute.tsx
+++ client/src/components/Pages/Auth/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Navigate, RouteProps } from "react-router-dom";
+import { Navigate, RouteProps, Outlet } from "react-router-dom";
 import { useSessionContext } from "supertokens-auth-react/recipe/session";
 
 interface PrivateRouteProps extends RouteProps {
@@ -10,7 +10,10 @@ export const PrivateRoute = ({ children }: PrivateRouteProps): JSX.Element => {
   const { doesSessionExist } = useSessionContext();
 
   return doesSessionExist ? (
-    <>{children}</>
+    <>
+      {children}
+      <Outlet />
+    </>
   ) : (
     <Navigate to="/auth/signin" replace />
   );

f30a22b92094c6e5bbea79ec47ab9a61872e1e25
Zaland Khan <zalandkhan1@gmail.com>
2023-07-01 21:58:12 -0300

    Switch to auth0 and replace supertokens

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,8 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import supertokens from "supertokens-node";
-import Session from "supertokens-node/recipe/session";
-import EmailPassword from "supertokens-node/recipe/emailpassword";
-import { middleware, errorHandler } from "supertokens-node/framework/express";
 import dotenv from "dotenv";
+import { auth } from "express-openid-connect";
 import UserRouter from "./routes/User.route";
 
 // init env values
@@ -14,22 +11,18 @@ dotenv.config();
 // init express app
 const app = express();
 
-// init supertokens
-supertokens.init({
-  framework: "express",
-  supertokens: {
-    connectionURI: process.env.CONNECTION_URI,
-    apiKey: process.env.API_KEY,
-  },
-  appInfo: {
-    appName: "FitNotes",
-    apiDomain: process.env.API_DOMAIN,
-    websiteDomain: process.env.CLIENT_DOMAIN,
-    apiBasePath: "/auth",
-    websiteBasePath: "/auth",
-  },
-  recipeList: [EmailPassword.init(), Session.init()],
-});
+// auth0 config
+const config = {
+  authRequired: process.env.ENVIRONMENT === "test" ? false : true,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 // bodyparser
 app.use(bodyParser.json());
@@ -38,18 +31,12 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: process.env.CLIENT_DOMAIN,
-    allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+    allowedHeaders: ["content-type"],
     credentials: true,
   })
 );
 
-// supertokens middleware
-app.use(middleware());
-
 // endpoints
 app.use("/user", UserRouter);
-
-// supertokens error handler
-app.use(errorHandler());
 
 export default app;
